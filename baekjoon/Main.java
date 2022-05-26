7ca9410188a10b2e58835babdd42d4cea0335a09
kimcno3 <kimcno3@naver.com>
2022-05-26 14:05:57 +0900

    백준 1927번 문제(최소힙) 문제 풀이

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -6,56 +6,20 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
         int length = sc.nextInt();
-
-        List<Integer> heap = new ArrayList<>();
-
-        for (int loop=0; loop<length; loop++) {
-            // 현재 숫자
-            Integer num = sc.nextInt();
-            // 힙이 비어있을 경우
-            if (heap.size() == 1) {
-                if (num == 0) {
-                     System.out.println(0);
-                } else {
-                    heap.add(num);
-                }
-                continue;
-            }
-
+        Integer num;
+        for (int i=0; i<length; i++) {
+            num = sc.nextInt();
             if (num != 0) {
-                // 마지막열에 추가
-                heap.add(num);
-                // 최소값 정렬 -> 비교는 부모노드만 -> 부모노드보다 작은 값이면 교환
-                while (num < heap.get(heap.lastIndexOf(num) / 2)) {
-                    Integer parentNode = heap.get(heap.lastIndexOf(num) / 2);
-                    heap.set(heap.lastIndexOf(num) / 2, num);
-                    heap.set(heap.get(heap.lastIndexOf(num)), parentNode);
+                heap.offer(num);
+            } else {
+                if (heap.size() == 0) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(heap.poll());
                 }
             }
-
-            /*
-            if (num == 0) {
-                // 최소값 제거 및 출력 -> 인덱스 0번 값을 출력
-                System.out.println(heap.get(0));
-                // 가장 마지막에 있는 값(최대값)을 1번으로
-                heap.set(1, heap.get(heap.size()-1));
-                heap.remove(heap.size()-1);
-                // 최소값 정렬 진행
-                Integer maxNum = heap.get(0);
-                while (heap.indexOf(maxNum)*2-1 < heap.size() && maxNum > heap.get(heap.indexOf(maxNum)*2-1)) {
-                    Integer childNode = heap.get(heap.indexOf(maxNum) * 2 - 1);
-                    heap.set(heap.indexOf(maxNum) * 2 - 1, maxNum);
-                    heap.set(heap.get(heap.indexOf(maxNum)), childNode);
-                }
-            }
-            */
-
-            // 테스트 코드 -> 힙을 순서대로 출력
-            for (int i=0; i<heap.size(); i++) {
-                System.out.print(heap.get(i) + " ");
-            }
-            System.out.println(heap.size());
         }
     }
 }
