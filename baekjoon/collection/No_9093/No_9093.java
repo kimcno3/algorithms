9247e128f1df9dc20e94b60a31ab1c278aeb66d5
kimcno3 <kimcno3@naver.com>
2022-05-27 15:18:14 +0900

    주석문 추가

--- baekjoon/collection/No_9093/No_9093.java
+++ baekjoon/collection/No_9093/No_9093.java
@@ -9,18 +9,25 @@ public class No_9093 {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    // 결과값을 담을 변수 선언
+    // append()를 활용하기 위해 StringBuffer 타입으로 선언
     StringBuffer result = new StringBuffer();
+    // 입력된 문장 개수
     int length = Integer.parseInt(br.readLine());
-
+    // 문장 반복
     for (int i=0; i<length; i++) {
-      String str = br.readLine();
-      StringTokenizer tokenizer = new StringTokenizer(str, " ");
-      while (tokenizer.hasMoreTokens()) {
-        StringBuffer word = new StringBuffer(tokenizer.nextToken());
+      // 띄어쓰기를 기준으로 단어를 구분한 배열 생성
+      String[] arr = br.readLine().split(" ");
+      // 단어 반복
+      for (int j=0; j<arr.length; j++) {
+        // StringBuffer로 형변환
+        StringBuffer word = new StringBuffer(arr[j]);
+        // 단어를 뒤집은 다음 result에 추가
         result.append(word.reverse() + " ");
       }
+      // result 출력 -> 뒤집은 단어를 합친 문장
       System.out.println(result);
+      // result 초기화
       result.delete(0, result.length());
     }
   }
