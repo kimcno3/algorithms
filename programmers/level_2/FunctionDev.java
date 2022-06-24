a87d4d317c847be22e2052c28649a3b4ddf210a3
kimcno3 <kimcno3@gmail.com>
2022-06-24 14:44:45 +0900

    프로그래머스 "기능개발" 문제 풀이

---
+++ programmers/level_2/FunctionDev.java
@@ -0,0 +1,52 @@
+package programmers.level_2;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class FunctionDev {
+
+  public static void main(String[] args) {
+    int[] progresses = {93, 30, 55};
+    int[] speeds = {1, 30, 5};
+
+    int[] answer = solution(progresses, speeds);
+
+    for (int i : answer) {
+      System.out.println(i);
+    }
+  }
+
+  public static int[] solution(int[] progresses, int[] speeds) {
+
+    HashMap<Integer, Double> map = new HashMap<>();
+    LinkedList<Integer> list = new LinkedList<>();
+
+    for (int i=0; i<progresses.length; i++) {
+      double days = Math.ceil((100.0-progresses[i])/speeds[i]);
+      map.put(i, days);
+    }
+
+    int count = 1;
+    double maxDays = map.get(0);
+
+    for (int i=1; i<map.size(); i++) {
+      if (map.get(i) <= maxDays) {
+        count++;
+      } else {
+        list.add(count);
+        maxDays = map.get(i);
+        count = 1;
+      }
+    }
+    list.add(count);
+
+    int[] answer = new int[list.size()];
+
+    for (int i=0; i<answer.length; i++) {
+      answer[i] = list.removeFirst();
+    }
+
+    return answer;
+
+  }
+}
