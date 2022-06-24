093964a7c9da92b0c190e29dbcce40a4b883083a
kimcno3 <kimcno3@gmail.com>
2022-06-24 14:50:19 +0900

    프로그래머스 "기능개발" 문제 주석문 추가

--- programmers/level_2/FunctionDev.java
+++ programmers/level_2/FunctionDev.java
@@ -3,18 +3,8 @@ package programmers.level_2;
 import java.util.HashMap;
 import java.util.LinkedList;
 
+// 기능 개발 : https://programmers.co.kr/learn/courses/30/lessons/42586
 public class FunctionDev {
-
-  public static void main(String[] args) {
-    int[] progresses = {93, 30, 55};
-    int[] speeds = {1, 30, 5};
-
-    int[] answer = solution(progresses, speeds);
-
-    for (int i : answer) {
-      System.out.println(i);
-    }
-  }
 
   public static int[] solution(int[] progresses, int[] speeds) {
 
@@ -48,5 +38,16 @@ public class FunctionDev {
 
     return answer;
 
+  }
+
+  public static void main(String[] args) {
+    int[] progresses = {93, 30, 55};
+    int[] speeds = {1, 30, 5};
+
+    int[] answer = solution(progresses, speeds);
+
+    for (int i : answer) {
+      System.out.println(i);
+    }
   }
 }
