0b92a46fb7e3fd882bfbe45b25e5902303751f25
kimcno3 <kimcno3@gmail.com>
2022-08-04 15:42:10 +0900

    프로그래머스 "다트게임" 문제 풀이(2차)

--- programmers/level_1/DartGame.java
+++ programmers/level_1/DartGame.java
@@ -7,15 +7,13 @@ public class DartGame {
 
   public static void main(String[] args) {
     DartGame dartGame = new DartGame();
-    System.out.println(dartGame.solution("1D2S#10S"));
+//    System.out.println(dartGame.solution("1D2S#10S"));
+    System.out.println(dartGame.solution2("1D2S0T"));
 
   }
-  /*
-  1차 작성 코드
 
-  부족했던 점
-    -
-  */
+
+  // 1차 작성 코드
   public int solution(String dartResult) {
     int answer = 0;
     String[] split = dartResult.split("[SDT*#]");
@@ -43,4 +41,56 @@ public class DartGame {
   }
 
   // 답안코드 https://yeoeun-ji.tistory.com/110
+
+  // 2차 작성 코드
+  public int solution2(String dartResult) {
+    int answer = 0;
+    String[] split = dartResult.split("");
+
+    String numStr = "";
+    int n;
+
+    // 라운드별 점수의 변경을 반영하고 마지막에 총합하기 위해 배열로 라운드별 점수를 저장해둔다.(중첩 결과를 반영하기 위해!!)
+    int[] round = new int[3];
+    int idx = 0;
+
+    for (String str : split) {
+      // 숫자인 경우
+      if (str.matches("[0-9]")) {
+        numStr += str;
+      }
+
+      // S, D, T 인 경우
+      else if (str.matches("[SDT]")) {
+        n = Integer.parseInt(numStr);
+        switch (str) {
+          case "S" :
+            round[idx++] = (int) Math.pow(n, 1);
+            break;
+          case "D" :
+            round[idx++] = (int) Math.pow(n, 2);
+            break;
+          case "T" :
+            round[idx++] = (int) Math.pow(n, 3);
+            break;
+        }
+        // 문자열만 초기화
+        numStr = "";
+      }
+      // 아차상, 스타상이 경우
+      else {
+        switch (str) {
+          case "*" :
+            round[idx-1] *= 2;
+            // 이전 라운드 결과에도 영향을 준다.
+            if(idx-2>=0) round[idx-2] *= 2;
+            break;
+          case "#" :
+            round[idx-1] *= (-1);
+            break;
+        }
+      }
+    }
+    return answer;
+  }
 }
