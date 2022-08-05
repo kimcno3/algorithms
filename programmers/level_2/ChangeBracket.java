89fb6aea5a6db6c3134be237c36dcd51d3682470
kimcno3 <kimcno3@gmail.com>
2022-08-05 15:56:33 +0900

    프로그래머스 "괄호 변환" 문제 풀이

---
+++ programmers/level_2/ChangeBracket.java
@@ -0,0 +1,82 @@
+package programmers.level_2;
+
+public class ChangeBracket {
+
+
+  public static void main(String[] args) {
+    ChangeBracket changeBracket = new ChangeBracket();
+    System.out.println(changeBracket.solution("(()())()"));
+    System.out.println(changeBracket.solution(")("));
+    System.out.println(changeBracket.solution("()))((()"));
+    System.out.println(changeBracket.solution("())()("));
+  }
+
+  public static StringBuilder sb;
+
+  public String solution(String p) {
+    sb = new StringBuilder();
+    method(p);
+    return sb.toString();
+  }
+
+  public void method(String p) {
+
+    // p가 빈 문자열일 경우 -> 더 이상 재귀를 돌 필요가 없다.
+    if (p.equals("")) {
+      return;
+    }
+
+    int open = 0;
+    int close = 0;
+
+    String u = "";
+    String v = "";
+
+    // u, v로 구분
+    for(int i=0; i<p.length(); i++) {
+
+      if (p.charAt(i) == '(') {
+        open++;
+      } else {
+        close++;
+      }
+
+      if (open == close) {
+        u = p.substring(0, i+1);
+        v = p.substring(i+1);
+        break;
+      }
+
+    }
+
+    // u가 올바른 문자열일 경우
+    if(u.charAt(0) == '(' && u.charAt(u.length()-1) == ')') {
+      sb.append(u);
+      method(v);
+    }
+
+    // u가 올바른 문자열이 아닌경우 -> 여기를 다시 해봐야 하겠다.
+    else {
+      sb.append("(");
+
+      // v를 가지고 재귀 시작
+      method(v);
+
+      // 앞,뒤 괄호 제거
+      u = u.subSequence(1, u.length()-1).toString();
+
+      // 괄호 방향 변경
+      String temp = "";
+      for (int i=0; i<u.length(); i++) {
+        if (u.charAt(i) == '(') temp += ")";
+        else temp += "(";
+      }
+
+      u = temp;
+
+      sb.append(u);
+      sb.append(")");
+    }
+
+  }
+}
