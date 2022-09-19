32fd9dd4dba6fe7215062611d9cdb04480b0edb3
kimcno3 <kimcno3@gmail.com>
2022-09-19 15:59:47 +0900

    프로그래머스 "JadenCase 문자열 만들기" 문제풀이

---
+++ programmers/level_2/JadenCase.java
@@ -0,0 +1,40 @@
+package programmers.level_2;
+
+import java.util.*;
+
+// JadenCase 만들기 : https://school.programmers.co.kr/learn/courses/30/lessons/12951
+public class JadenCase {
+
+  public static void main(String[] args) {
+    JadenCase jadenCase = new JadenCase();
+    System.out.println(jadenCase.solution("people unFollowed me"));
+    System.out.println(jadenCase.solution("for the last week"));
+    System.out.println(jadenCase.solution("i am iron man  "));
+  }
+
+  public String solution(String s) {
+
+    String answer = "";
+    // 문자열 전체를 소문자로 변경하고 split
+    String[] arr = s.toLowerCase(Locale.ROOT).split(" ");
+
+    for (String str : arr) {
+      // 첫번째 문장을 변수에 저장
+      String firstStr = str.split("")[0];
+      // 첫번째 문장이 나오는 가장 첫번째 인덱스의 문자열만 대문자로 변경
+      str = str.replaceFirst(firstStr, firstStr.toUpperCase(Locale.ROOT));
+      // 빈칸과 함께 answer에 추가
+      answer += str + " ";
+
+    }
+    // answer의 문자열 길이가 s보다 길다면 이전 빈칸 문자가 하나 추가되었다는 의미
+    if (answer.length() > s.length()) {
+      // 해당 조건에서만 trim()으로 앞뒤 빈킨 문자를 제거
+      answer = answer.trim();
+    }
+
+    return answer;
+
+  }
+
+}
