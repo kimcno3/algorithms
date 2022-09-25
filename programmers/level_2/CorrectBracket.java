b3e491fde940051c95fac03ba0f57296aaf2274f
kimcno3 <kimcno3@gmail.com>
2022-09-25 18:28:49 +0900

    프로그래머스 "올바른 괄호" 주석문 설명 추가
    * split(), charAt() 효율성 차이에 대한 견해

--- programmers/level_2/CorrectBracket.java
+++ programmers/level_2/CorrectBracket.java
@@ -5,7 +5,11 @@ import java.util.Stack;
 /* 올바른 괄호 : https://school.programmers.co.kr/learn/courses/30/lessons/12909
  *
  * 몇가지 아쉬운 부분
-   * split()을 활용해 문자열을 배열로 만들지 말고 charAt()을 활용해야 한다. -> 효율성 테스트 실패의 원인
+   * split()을 활용해 문자열을 배열로 만들지 말고 charAt()을 활용해야 한다.
+     * 효율성 테스트 실패의 원인!!
+     * charAt() 내부 구현 코드를 보면 String 객체에 저장된 byte[] 배열을 활용해 그대로 값을 리턴
+     * split()은 복잡한 로직 과정을 거친다.(결국 새로운 배열을 만드내는 과정)
+
    * Stack 문제는 LinkedList 가 아니라 Stack 자료구조를 사용하는 것을 추천한다.
    * LinkedList 계열 자료구조는 empty()가 있다.
  */
