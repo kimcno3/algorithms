30a1de1947b9e9065dc1a68e517e75d4b2ddde3a
kimcno3 <kimcno3@gmail.com>
2023-08-29 23:01:44 +0900

    프로그래머스 "카펫" 풀이 추가

--- programmers/level_2/Carpet.java
+++ programmers/level_2/Carpet.java
@@ -1,6 +1,7 @@
 package programmers.level_2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 // 카펫 : https://school.programmers.co.kr/learn/courses/30/lessons/42842
 
@@ -10,6 +11,31 @@ import java.util.ArrayList;
  */
 
 public class Carpet {
+
+  public int[] solutionV2(int brown, int yellow) {
+
+    List<int[]> list = new ArrayList<>();
+    int sum = brown + yellow;
+    // 가로가 길거나 같기 때문에 i는 세로로 본다.
+    for (int i=3; i<=Math.sqrt(sum); i++) {
+      // 합 나누기 세로의 나머지가 0이고 몫 * 세로가 합이라면 리스트에 추가(가로, 세로)
+      if (sum % i == 0 && i * (sum/i) == sum) list.add(new int[]{sum/i, i});
+    }
+    // 리스트 루프(정답 후보들)
+    for (int[] data : list) {
+      // 임시 노란 카펫
+      int tempYellow = (data[0] - 2) * (data[1] - 2);
+      // 임시 갈색 카펫
+      int tempBrown = (data[0] * data[1]) - tempYellow;
+
+      // 정답 찾기
+      if (tempBrown == brown && tempYellow == yellow) {
+        // 정답을 answer로 리턴
+        return data;
+      }
+    }
+    return new int[2];
+  }
 
   public int[] solution(int brown, int yellow) {
 
