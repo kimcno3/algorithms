72e4f0dcd8fa4ff7b8054de87e612bf01d1bf2a4
kimcno3 <kimcno3@gmail.com>
2023-09-08 15:57:13 +0900

    이진탐색 최대힙 최소힙 구하는 로직 추가

--- programmers/level_2/RankSearch.java
+++ programmers/level_2/RankSearch.java
@@ -11,7 +11,7 @@ import java.util.*;
  - 이런 경우, 생성될 수 있는 모든 경우에 수에 대해서 해시 키로 등록을 하는 방법을 선택
  - 이후 각 키에 저장될 숫자에 대한 값을 List에 저장
  - 모든 저장이 완료된 이후, List에 저장된 값을 일괄적으로 정렬해준다.
- - 정렬된 이후, 각 List를 탐색하는 과정에서 일정 값 이상의 경우에 대한 개수만 알면 되닌 이진 탐색으로 효율성을 높힌다.
+ - 정렬된 이후, 각 List를 탐색하는 과정에서 일정 값 이상의 경우에 대한 개수만 알면 되니 이진 탐색으로 효율성을 높힌다.
  2. 풀이 과정 속에서 시간 복잡도에 대한 고민을 해봐야 한다.
  3. 내가 막힌 부분이나 고민이 되는 부분은 말로 해보던지 적어보면서 정리해보는 시간을 가져보면서 진행해본다.
  4. 반복되는 코드나 코드 흐름상 분리해줘야 할 필요가 있다면 함수로 분리해주는 필요가 있다.
@@ -46,7 +46,7 @@ public class RankSearch {
 
       int idx = binarySearch(list, score); // 이진 탐색 메소드 구현
 
-      // Collections.binarySearch(list, score); -> Collections 클래스에서 제공해주는 이진 탐색 메소
+      // Collections.binarySearch(list, score); -> Collections 클래스에서 제공해주는 이진 탐색 메소드
 
       if (idx == -1) {
         idx = list.size();
@@ -84,8 +84,11 @@ public class RankSearch {
     }
 
     return key;
+
+
   }
 
+  // 최대값 구하기
   public int binarySearch(List<Integer> list, int score) {
     int low = 0;
     int high = list.size()-1;
@@ -99,10 +102,33 @@ public class RankSearch {
       if (midScore < score) {
         low = mid + 1;
       } else {
-        result = mid; // !!
+        result = mid; // 큰 값이 나온 경우 해당 인덱스를 수시로 업데이트
         high = mid - 1;
       }
     }
+    return result;
+  }
+
+  // 최소값 구하기
+  private int binarySearch(int[] scoville, int K) {
+    int low = 0;
+    int high = scoville.length - 1;
+    int result = 0;
+
+    while(low <= high) {
+
+      int mid = (low + high) / 2;
+
+      if (scoville[mid] < K) {
+        result = mid; // 작은 경우 해당 인덱스를 수시로 업데이트
+        low = mid + 1;
+      }
+      else if(scoville[mid] > K) {
+        high = mid - 1;
+      }
+      else return mid;
+    }
+
     return result;
   }
 
