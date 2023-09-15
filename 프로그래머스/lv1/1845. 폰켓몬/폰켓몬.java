20036c9a4c1e32e47c36214fa694023bb823fb2a
Seonho Kim <77563468+kimcno3@users.noreply.github.com>
2023-09-15 13:37:38 +0900

    [level 1] Title: 폰켓몬, Time: 3.10 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+    int answer = 0;
+    HashSet<Integer> set = new HashSet<>();
+
+    for (int num : nums) {
+      set.add(num);
+    }
+
+    if (nums.length/2 < set.size()) {
+      answer = nums.length/2;
+    } else {
+      answer = set.size();
+    }
+    
+    return answer;
+    }
+}
