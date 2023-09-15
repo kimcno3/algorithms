7a9ab8c21ece8ab0c82d39ccf0944276ba4ae7b3
Seonho Kim <77563468+kimcno3@users.noreply.github.com>
2023-09-15 13:13:48 +0900

    [Silver III] Title: The Big Dance, Time: 120 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/6012. The Big Dance/The Big Dance.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// The Big Dance : https://www.acmicpc.net/problem/6012
+public class Main {
+
+  static int[] arr;
+  static int sum;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    arr = new int[n];
+
+    for (int i=0; i<arr.length; i++) {
+      arr[i] = i+1;
+    }
+
+    method(0, n);
+    System.out.println(sum);
+  }
+
+  public static void method(int sIdx, int size) {
+    if (size == 1) {
+      return;
+    }
+    if (size == 2) {
+      sum += arr[sIdx] * arr[sIdx+1];
+      return;
+    }
+
+    int newSize = (int) Math.ceil((double) (size)/2);
+
+    method(sIdx, newSize);
+    method(sIdx+newSize, size-newSize);
+  }
+
+}
