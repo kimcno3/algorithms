34d450c134de89f6607f95c8ca7d8d2d5623c683
Seonho Kim <77563468+kimcno3@users.noreply.github.com>
2023-09-15 15:50:22 +0900

    [level 3] Title: 정수 삼각형, Time: 26.92 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -12,28 +12,24 @@ class Solution {
         for (int[] arr :dp) {
             Arrays.fill(arr, -1);
         }
-
         dp[0][0] = triangle[0][0];
-
-        // dp 생성
+        // 순차적으로 최대값을 저장
         for (int y=1; y<triangle.length; y++) {
             for (int x=0; x<triangle[y].length; x++) {
                 if (dp[y - 1][x] != -1) {
                     dp[y][x] = Math.max(dp[y][x], triangle[y][x] + dp[y - 1][x]);
                 }
-
                 if (x < 1) continue;
-
                 if (dp[y - 1][x - 1] != -1) {
                     dp[y][x] = Math.max(dp[y][x], triangle[y][x] + dp[y - 1][x - 1]);
                 }
             }
         }
-
+        // 가장 하단에 저장된 값들 중 최대 값을 선정
         for (int num : dp[dp.length-1]) {
             answer = Math.max(num, answer);
         }
-
+        // 결과 리턴
         return answer;
     }
 
