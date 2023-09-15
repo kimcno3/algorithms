eac8547978f54155f2124870591696fdb51eba10
Seonho Kim <77563468+kimcno3@users.noreply.github.com>
2023-09-15 13:43:03 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;
+
+class Solution {
+    public double solution(int[] arr) {
+        int sum = 0;
+        for (int n : arr) {
+            sum += n;
+        }
+        return (double) sum / arr.length;
+    }
+}
