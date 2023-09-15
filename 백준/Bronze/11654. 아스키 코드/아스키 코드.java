79fbc9f3d5bffb74a834943879f169f77f185294
Seonho Kim <77563468+kimcno3@users.noreply.github.com>
2023-09-15 13:35:39 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((int) bf.readLine().charAt(0));
+    }
+}
