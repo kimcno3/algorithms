229ddbdda0967c34fbc980c21879a35ddc127b89
Seonho Kim <77563468+kimcno3@users.noreply.github.com>
2023-09-15 13:15:34 +0900

    [Gold IV] Title: N-Queen, Time: 5412 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 public class Main {
 
   static int n, result;
+
+  //static boolean[][] arr;
   static int[] arr;
 
   public static void main(String[] args) throws IOException {
@@ -12,14 +14,77 @@ public class Main {
 
     n = Integer.parseInt(br.readLine());
     result = 0;
+
+    // arr = new boolean[n][n];
     arr = new int[n];
 
     method(0);
 
     System.out.println(result);
-      
   }
-    
+
+  /*
+  // 1차 문제 풀이
+  public static void method(int x, int y, int depth) {
+
+    if (depth == n) {
+      result++;
+      return;
+    }
+
+    // 상하좌우 비교 로직
+    boolean flag = check1(x, y);
+
+    // 문제1: 대각선 비교 로직을 구현하지 못함.
+
+    if (flag) {
+      arr[x][y] = true;
+    }
+
+    // 문제2 : 재귀함수를 호출하기 위한 조건을 만들어내지 못함.
+
+    arr[x][y] = false;
+
+  }
+  public static boolean check1(int x, int y) {
+
+    // 상
+    for (int i=x; i>=0; i--) {
+      if (arr[i][y]) {
+        return false;
+      }
+    }
+
+    // 하
+    for (int i=x; i<n; i++) {
+      if (arr[i][y]) {
+        return false;
+      }
+    }
+
+    // 좌
+    for (int i=y; i>=0; i--) {
+      if (arr[x][i]) {
+        return false;
+      }
+    }
+
+    // 우
+    for (int i=y; i<n; i++) {
+      if (arr[x][i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+  */
+
+  // 답안 코드 : https://st-lab.tistory.com/118
+  // 힌트1 : 2차 배열이 아닌 1차 배열로 행과 열을 표현해보기 -> 같은 행에 위치한 녀셕들은 비교대상에서 제외할 수 있을 듯
+  // 힌트2 : 대각선은 열간의 차와 행간의 차가 같다면 대각선에 위치해 있다고 볼 수 있다.
+
+  // 2차 문제 풀이 -> 거의 비슷한 로직을 구현했지만 답안 코드를 참고하고 이해하는 방향으로 작성했습니다.
   public static void method(int depth) {
 
     if (depth == n) {
