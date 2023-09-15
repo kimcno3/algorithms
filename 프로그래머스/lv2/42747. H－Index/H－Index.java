c39783cc5761bc5a24847f0562b549040c89a28d
Seonho Kim <77563468+kimcno3@users.noreply.github.com>
2023-09-15 15:20:57 +0900

    [level 2] Title: H-Index, Time: 0.36 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+/**
+ * 참고 자료 : https://hyojun.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-H-Index-Java
+ */
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        // 오름차순 정렬
+        Arrays.sort(citations);
+        for(int i = 0; i < citations.length; i++) {
+            // 인용된 논문의 수
+            int h = citations.length - i;
+            // 인용된 논문의 수보다 해당 인덱스의 논문의 인용수가 크거나 같아야 한다.
+            if(citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+        return answer;
+    }
+}
