8133a7b9ed0fe193d31b40bba2bbfc9175876178
Seonho Kim <77563468+kimcno3@users.noreply.github.com>
2023-09-15 13:58:57 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.07 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,19 +1,22 @@
-class Solution {
-    public int solution(int[] numbers) {
-        int answer = 0;
-        StringBuilder sb = new StringBuilder();
+public class Solution {
+ 
+  // solution
+  public static int solution(int[] numbers) {
+    int answer = 0;
+    StringBuilder sb = new StringBuilder();
 
-        for (int number : numbers) {
-          sb.append(number);
-        }
-
-        for (int i=0; i<=9; i++) {
-          if (!sb.toString().contains(String.valueOf(i))) {
-            answer += i;
-          }
-        }
-
-        return answer;
-        
+    // int 배열 값을 StringBuilder 로 치환
+    for (int number : numbers) {
+      sb.append(number);
     }
+
+    // 0 ~ 9까지의 숫자 중 포함이 안된 숫자만 더한다.
+    for (int i=0; i<=9; i++) {
+      if (!sb.toString().contains(String.valueOf(i))) {
+        answer += i;
+      }
+    }
+
+    return answer;
+  }
 }
