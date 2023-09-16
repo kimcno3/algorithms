57533db2573b7c5d6e169dfb842f15d0a659c03e
Seonho Kim <77563468+kimcno3@users.noreply.github.com>
2023-09-16 14:37:39 +0900

    [Silver III] Title: N과 M (4), Time: 196 ms, Memory: 18964 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+// N과 M(4) : https://www.acmicpc.net/problem/15652
+public class Main {
+
+  static int n, m;
+  static int[] arr;
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] split = br.readLine().split(" ");
+
+    n = Integer.parseInt(split[0]);
+    m = Integer.parseInt(split[1]);
+
+    arr = new int[m];
+
+    method(1, 0);
+    System.out.println(sb);
+
+  }
+
+  public static void method(int sp, int depth) {
+
+    if(depth == m) {
+      for (int val : arr) {
+        sb.append(val + " ");
+      }
+      sb.append("\n");
+      return;
+    }
+
+    for (int i=sp; i<=n; i++) {
+      arr[depth] = i;
+      method(i, depth+1);
+    }
+
+  }
+  
+}
